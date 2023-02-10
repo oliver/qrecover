e8a938c3d7eb85f0fc2f16e44cf05969e295fab7
Oliver Gerlich <oliver.gerlich@gmx.de>
2023-02-10 21:31:44 +0100

    popup for replacement suggestions is now filled lazily
    This improves interactive performance with alphanumeric codes.

--- area_details_listbox.js
+++ area_details_listbox.js
@@ -79,52 +79,52 @@ function update_area_details_list (decoder) {
                     show_correction(null);
                 }, false);
             } else if (area.value_details.replacement_candidates && area.value_details.replacement_candidates.length > 0) {
-                const popup_div = document.createElement("div");
-
                 cell.innerHTML += area.value_details.desc + " <button>Show Values...</button>";
                 const button = cell.querySelector("button");
                 button.addEventListener("click", function (e) {
+                    const popup_div = document.createElement("div");
                     document.querySelector("#area_table_div").appendChild(popup_div);
+
+                    popup_div.innerHTML = "<button style='float:right'>X</button><h3>Possible Values</h3>Current: " + area.value_details.desc + "<ul></ul>";
+                    popup_div.style.position = "absolute";
+                    popup_div.style.backgroundColor = "white";
+                    popup_div.style.borderRadius = "10px";
+                    popup_div.style.boxShadow = "0px 0px 10px 0px black"
+                    popup_div.style.padding = "1em";
+                    popup_div.style.maxHeight = "80%";
+                    popup_div.style.overflowY = "scroll";
+
+                    const close_button = popup_div.querySelector("button");
+                    close_button.addEventListener("click", function (e) {
+                        document.querySelector("#area_table_div").removeChild(popup_div);
+                    }, false);
+
+                    var list_element = popup_div.querySelector("ul");
+                    for (let replacement_candidate of area.value_details.replacement_candidates) {
+                        const new_sub_item = document.createElement("li");
+                        new_sub_item.innerHTML = "<span style='text-decoration: underline dashed;'>" + replacement_candidate.desc + " <button>Apply</button></span>";
+                        list_element.appendChild(new_sub_item);
+
+                        const span = new_sub_item.querySelector("span");
+                        span.addEventListener("mouseover", function (e) {
+                            show_correction(replacement_candidate.replacements);
+                        }, false);
+                        span.addEventListener("mouseout", function (e) {
+                            show_correction(null);
+                        }, false);
+
+                        const button = new_sub_item.querySelector("button");
+                        button.addEventListener("click", function (e) {
+                            apply_correction(replacement_candidate.replacements);
+                            show_correction(null);
+                            document.querySelector("#area_table_div").removeChild(popup_div);
+                        }, false);
+                    }
+
                     const button_offset = get_offset_relative_to(button, document.querySelector("#area_table_div"));
                     popup_div.style.top = (button_offset.top - ((popup_div.clientHeight - button.clientHeight) / 2)) + "px";
                     popup_div.style.left = (button_offset.left - popup_div.clientWidth - 20) + "px";
                 }, false);
-
-                popup_div.innerHTML = "<button style='float:right'>X</button><h3>Possible Values</h3>Current: " + area.value_details.desc + "<ul></ul>";
-                popup_div.style.position = "absolute";
-                popup_div.style.backgroundColor = "white";
-                popup_div.style.borderRadius = "10px";
-                popup_div.style.boxShadow = "0px 0px 10px 0px black"
-                popup_div.style.padding = "1em";
-                popup_div.style.maxHeight = "80%";
-                popup_div.style.overflowY = "scroll";
-
-                const close_button = popup_div.querySelector("button");
-                close_button.addEventListener("click", function (e) {
-                    document.querySelector("#area_table_div").removeChild(popup_div);
-                }, false);
-
-                var list_element = popup_div.querySelector("ul");
-                for (let replacement_candidate of area.value_details.replacement_candidates) {
-                    const new_sub_item = document.createElement("li");
-                    new_sub_item.innerHTML = "<span style='text-decoration: underline dashed;'>" + replacement_candidate.desc + " <button>Apply</button></span>";
-                    list_element.appendChild(new_sub_item);
-
-                    const span = new_sub_item.querySelector("span");
-                    span.addEventListener("mouseover", function (e) {
-                        show_correction(replacement_candidate.replacements);
-                    }, false);
-                    span.addEventListener("mouseout", function (e) {
-                        show_correction(null);
-                    }, false);
-
-                    const button = new_sub_item.querySelector("button");
-                    button.addEventListener("click", function (e) {
-                        apply_correction(replacement_candidate.replacements);
-                        show_correction(null);
-                        document.querySelector("#area_table_div").removeChild(popup_div);
-                    }, false);
-                }
             } else {
                 cell.innerHTML = area.value_details.desc;
             }
