475aad1fa91f3776a8230e23f85fb3ab46814070
Oliver Gerlich <oliver.gerlich@gmx.de>
2023-02-12 12:01:50 +0100

    initial snapshot of picture loading experiments
    The final goal here is to show a picture of the original QR code (from a
    photo) and let the user drag the QR code "frame" over the picture in the
    correct place.
    Later, the user-specified picture area could be displayed in the main
    canvas (below the pixel data), as reference for the user.
    
    These are two (alternative) experimental approaches in this snapshot:
    - a Canvas area. It shows a QR code frame, and it can show the loaded
      picture.
    - an SVG area. It show the QR code frame and some draggable corner markers.
    
    The SVG approach has the advantage that mouse interaction appears to be
    much easier than with a Canvas.

---
+++ picture_dialog.js
@@ -0,0 +1,129 @@
+//
+// Popup dialog for loading and tracing a QR code from a picture
+//
+
+function open_picture_dialog () {
+    new PictureDialog();
+}
+
+class PictureDialog {
+    constructor () {
+        this.popup = create_popup_dialog(document.body);
+        this.popup.insertAdjacentHTML("beforeend", '<h3>Load Picture</h3>\
+            <input type="file" id="picture_file_input" accept="image/*" />\
+            <input type="button" id="picture_load_button" value="Load Selected File">\
+            <br>\
+            <svg id="picture_svg" width="50%" height="50%"></svg> \
+            <canvas id="picture_canvas" style="width: 100%; height: 85%; background-color: antiquewhite;"></canvas>\
+            ');
+        this.canvas = this.popup.querySelector("#picture_canvas");
+
+        this.popup.style.left = "10ex";
+        this.popup.style.right = "10ex";
+        this.popup.style.top = "10ex";
+        this.popup.style.bottom = "10ex";
+
+        const picture_dialog = this;
+        this.popup.querySelector("#picture_load_button").addEventListener("click", () => {
+            const [file] = this.popup.querySelector("#picture_file_input").files;
+            if (file) {
+                const img_obj = new Image();
+                img_obj.src = URL.createObjectURL(file);
+                img_obj.onload = function () {
+                    picture_dialog.load_picture(img_obj);
+                }
+            }
+        });
+
+        // adjust canvas pixel size to its DOM element size:
+        const canvas_rect = this.canvas.getBoundingClientRect();
+        this.canvas.width = canvas_rect.width;
+        this.canvas.height = canvas_rect.height;
+
+
+        const svg = this.popup.querySelector("#picture_svg");
+
+        function svg_add_circle (svg, cx, cy, r) {
+            const elem = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+            elem.setAttribute("cx", cx);
+            elem.setAttribute("cy", cy);
+            elem.setAttribute("r", r);
+            svg.append(elem);
+            return elem;
+        }
+        function svg_add_line (svg, x1, y1, x2, y2, stroke) {
+            const elem = document.createElementNS("http://www.w3.org/2000/svg", "line");
+            elem.setAttribute("x1", x1);
+            elem.setAttribute("y1", y1);
+            elem.setAttribute("x2", x2);
+            elem.setAttribute("y2", y2);
+            elem.setAttribute("stroke", stroke);
+            svg.append(elem);
+            return elem;
+        }
+
+        function event_parent_coords (evt) {
+            const parent_bounds = evt.target.parentElement.getBoundingClientRect();
+            return [evt.clientX - parent_bounds.left, evt.clientY - parent_bounds.top];
+        }
+
+        var corners = [ [100,100], [200,100], [200,200], [100,200] ];
+
+        for (var i = 0; i < 4; i++) {
+            const next_i = (i+1) % 4;
+            svg_add_line(svg, corners[i][0], corners[i][1], corners[next_i][0], corners[next_i][1], "black");
+        }
+
+        for (var i = 0; i < 4; i++) {
+            const circle = svg_add_circle(svg, corners[i][0], corners[i][1], 10);
+            circle.addEventListener("pointerover", (evt) => {
+                evt.target.style.fill = "red";
+            });
+            circle.addEventListener("pointerout", (evt) => {
+                evt.target.style.fill = null;
+            });
+
+            circle.drag_active = false;
+            circle.addEventListener("pointerdown", (evt) => {
+                evt.target.setPointerCapture(evt.pointerId);
+                evt.target.drag_active = true;
+            });
+            circle.addEventListener("pointerup", (evt) => {
+                evt.target.releasePointerCapture(evt.pointerId);
+                evt.target.drag_active = false;
+            });
+            circle.addEventListener("pointermove", (evt) => {
+                if (evt.target.drag_active) {
+                    const [x, y] = event_parent_coords(evt);
+                    evt.target.setAttribute("cx", x);
+                    evt.target.setAttribute("cy", y);
+                }
+            });
+        }
+
+        this.draw();
+    }
+
+    load_picture(img_obj) {
+        console.log("load_picture; img_obj:", img_obj);
+
+        this.canvas.getContext("2d").drawImage(img_obj, 0, 0, img_obj.width, img_obj.height);
+        this.draw();
+    }
+
+    draw () {
+        const ctx = this.canvas.getContext("2d");
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 1;
+        ctx.setLineDash([3, 3]);
+
+        var corners = [ [100,100], [200,100], [200,200], [100,200] ];
+
+        ctx.beginPath();
+        ctx.moveTo(corners[3][0], corners[3][1]);
+        for (var i = 0; i < 4; i++) {
+            ctx.lineTo(corners[i][0], corners[i][1]);
+        }
+        ctx.stroke();
+    }
+}
