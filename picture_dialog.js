bf4b622f31b3751c31e421336f7336e82c0cbc66
Oliver Gerlich <oliver.gerlich@gmx.de>
2023-02-12 15:57:33 +0100

    use frame corners to actually transform the loaded picture
    This uses the code from https://gist.github.com/fta2012/bd63f7fd9f385870efc0
    and the underlying numeric.js library to do the heavy lifting.

--- picture_dialog.js
+++ picture_dialog.js
@@ -13,8 +13,8 @@ class PictureDialog {
             <input type="file" id="picture_file_input" accept="image/*" />\
             <input type="button" id="picture_load_button" value="Load Selected File">\
             <br>\
-            <svg id="picture_svg" width="50%" height="50%"></svg> \
-            <canvas id="picture_canvas" style="width: 100%; height: 85%; background-color: antiquewhite;"></canvas>\
+            <svg id="picture_svg" width="50%" height="50%" style="border: solid 1px black"></svg><br> \
+            <canvas id="picture_canvas" style="width: 30%; height: 30%; background-color: antiquewhite; position: absolute; right: 0px"></canvas>\
             ');
         this.canvas = this.popup.querySelector("#picture_canvas");
 
@@ -49,15 +49,21 @@ class PictureDialog {
             return [evt.clientX - parent_bounds.left, evt.clientY - parent_bounds.top];
         }
 
-        var corners = [ [100,100], [200,100], [200,200], [100,200] ];
+        this.original_corners = [ [0,0], [100,0], [100,100], [0,100] ];
+        this.corners = [ [100,100], [200,100], [200,200], [100,200] ];
+
+        if (sessionStorage.getItem("picture_corners")) {
+            this.corners = JSON.parse(sessionStorage.getItem("picture_corners"));
+        }
+        applyTransform(this.canvas, this.corners, this.original_corners, null);
 
         for (var i = 0; i < 4; i++) {
             const next_i = (i+1) % 4;
-            svg_add_line(this.svg, corners[i][0], corners[i][1], corners[next_i][0], corners[next_i][1], "black");
+            svg_add_line(this.svg, this.corners[i][0], this.corners[i][1], this.corners[next_i][0], this.corners[next_i][1], "black");
         }
 
         for (var i = 0; i < 4; i++) {
-            const circle = svg_add_circle(this.svg, corners[i][0], corners[i][1], 10);
+            const circle = svg_add_circle(this.svg, this.corners[i][0], this.corners[i][1], 10);
             circle.style.fill = "rgba(0,0,0,0.5)";
             circle.addEventListener("pointerover", (evt) => {
                 evt.target.style.fill = "rgba(255,0,0,0.5)";
@@ -65,6 +71,8 @@ class PictureDialog {
             circle.addEventListener("pointerout", (evt) => {
                 evt.target.style.fill = "rgba(0,0,0,0.5)";
             });
+
+            circle.corner_index = i;
 
             circle.drag_active = false;
             circle.addEventListener("pointerdown", (evt) => {
@@ -80,6 +88,10 @@ class PictureDialog {
                     const [x, y] = event_parent_coords(evt);
                     evt.target.setAttribute("cx", x);
                     evt.target.setAttribute("cy", y);
+
+                    this.corners[evt.target.corner_index] = [x, y];
+                    sessionStorage.setItem("picture_corners", JSON.stringify(this.corners));
+                    applyTransform(this.canvas, this.corners, this.original_corners, null);
                 }
             });
         }
@@ -134,3 +146,76 @@ function svg_add_circle (svg, cx, cy, r) {
 function svg_add_line (svg, x1, y1, x2, y2, stroke) {
     return svg_add_element(svg, "line", {"x1": x1, "y1": y1, "x2": x2, "y2": y2, "stroke": stroke});
 }
+
+
+getTransform = function(from, to) {
+  var A, H, b, h, i, k_i, lhs, rhs, _i, _j, _k, _ref;
+  console.assert((from.length === (_ref = to.length) && _ref === 4));
+  A = [];
+  for (i = _i = 0; _i < 4; i = ++_i) {
+    A.push([from[i].x, from[i].y, 1, 0, 0, 0, -from[i].x * to[i].x, -from[i].y * to[i].x]);
+    A.push([0, 0, 0, from[i].x, from[i].y, 1, -from[i].x * to[i].y, -from[i].y * to[i].y]);
+  }
+  b = [];
+  for (i = _j = 0; _j < 4; i = ++_j) {
+    b.push(to[i].x);
+    b.push(to[i].y);
+  }
+  h = numeric.solve(A, b);
+  H = [[h[0], h[1], 0, h[2]], [h[3], h[4], 0, h[5]], [0, 0, 1, 0], [h[6], h[7], 0, 1]];
+  for (i = _k = 0; _k < 4; i = ++_k) {
+    lhs = numeric.dot(H, [from[i].x, from[i].y, 0, 1]);
+    k_i = lhs[3];
+    rhs = numeric.dot(k_i, [to[i].x, to[i].y, 0, 1]);
+    console.assert(numeric.norm2(numeric.sub(lhs, rhs)) < 1e-9, "Not equal:", lhs, rhs);
+  }
+  return H;
+};
+
+
+applyTransform = function(element, originalPos, targetPos, callback) {
+  var H, from, i, j, p, to;
+  from = (function() {
+    var _i, _len, _results;
+    _results = [];
+    for (_i = 0, _len = originalPos.length; _i < _len; _i++) {
+      p = originalPos[_i];
+      _results.push({
+        x: p[0],
+        y: p[1]
+      });
+    }
+    return _results;
+  })();
+  to = (function() {
+    var _i, _len, _results;
+    _results = [];
+    for (_i = 0, _len = targetPos.length; _i < _len; _i++) {
+      p = targetPos[_i];
+      _results.push({
+        x: p[0],
+        y: p[1]
+      });
+    }
+    return _results;
+  })();
+  H = getTransform(from, to);
+
+  const transform_matrix_string = ((function() {
+    var _i, _results;
+    _results = [];
+    for (i = _i = 0; _i < 4; i = ++_i) {
+      _results.push((function() {
+        var _j, _results1;
+        _results1 = [];
+        for (j = _j = 0; _j < 4; j = ++_j) {
+          _results1.push(H[j][i].toFixed(20));
+        }
+        return _results1;
+      })());
+    }
+    return _results;
+  })()).join(',');
+  element.style.transform = "matrix3d(" + transform_matrix_string + ")";
+  element.style.transformOrigin = "0 0";
+};
